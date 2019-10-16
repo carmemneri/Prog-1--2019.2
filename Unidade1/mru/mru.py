71b999cf3577d992ca30b64d69252bd8c6bd9efe
carmemneri <54044906+carmemneri@users.noreply.github.com>
2019-10-16 17:02:48 -0300

    Update mru.py

--- Unidade1/mru/mru.py
+++ Unidade1/mru/mru.py
@@ -3,12 +3,12 @@
 #Juan Salvador da Silva -119210821
 #MRU
 
-posicao_inicial = float(input())
-velocidade = float(input())
-tempo = float(input())
+posicao_inicial = float(input()) #essa linha é para receber a posição que o usuário determinar
+velocidade = float(input()) #essa linha é para receber a velocidade que o usuário determiar
+tempo = float(input()) #essa linha é para receber o tempo que o usuário determinar
 
-posicao_final = posicao_inicial + (velocidade*tempo)
+posicao_final = posicao_inicial + (velocidade*tempo) #essa linha é resposável pelo cálculo da posição final
 
-print ("Posição final do móvel")
-print ("S(%.1f) = %.1f m" % (tempo, posicao_final))
+print ("Posição final do móvel") #essa linha representa a saída que o usuário espera
+print ("S(%.1f) = %.1f m" % (tempo, posicao_final)) #essa linha representa a saída que o usuário espera
 
